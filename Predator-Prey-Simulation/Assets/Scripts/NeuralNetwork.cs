1ef9fec097b280e02fab13b00d25c1ebdeb9e4f1
Upsidedrop <96622429+Upsidedrop@users.noreply.github.com>
2023-02-27 17:34:18 -0500

    Update 1.06
    Alright, here we go. I removed thrust as an input, I edited some comments, I created a method to calculate the value of each node using weights and biases, I made direction and speed functional as outputs, I removed the layer variable in the node struct, and I moved reproduction into it's own method. I got a lot done.

--- Predator-Prey-Simulation/Assets/Scripts/NeuralNetwork.cs
+++ Predator-Prey-Simulation/Assets/Scripts/NeuralNetwork.cs
@@ -12,7 +12,7 @@ public class NeuralNetwork : MonoBehaviour
     bool isPrey = false;
     bool isPredator = false;
     Rigidbody2D Rigidbody2D;
-    int thrust;
+    float thrust;
     float[] inputs = new float[5];
     [SerializeField]
     GameObject closestGameobject;
@@ -38,29 +38,11 @@ public class NeuralNetwork : MonoBehaviour
     {
         if (progressToReproduce >= 3)
         {
-            progressToReproduce = 0;
-            if (isPrey)
-            {
-                Instantiate(SpawnOGAnimals.prey,
-                    transform.position
-                    + Vector3.up
-                    * UnityEngine.Random.Range(-0.1f, 0.1f)
-                    + Vector3.right
-                    * UnityEngine.Random.Range(-0.1f, 0.1f),
-                    Quaternion.identity, SpawnOGAnimals.emptyPrey);
-            }
-            if (isPredator)
-            {
-                Instantiate(SpawnOGAnimals.predator,
-                    transform.position
-                    + Vector3.up
-                    * UnityEngine.Random.Range(-0.1f, 0.1f)
-                    + Vector3.right
-                    * UnityEngine.Random.Range(-0.1f, 0.1f),
-                    Quaternion.identity, SpawnOGAnimals.emptyPredator);
-            }
+            Reproduce();
 
         }
+
+        Rigidbody2D.velocity = transform.up * thrust;
         AssignInputs();
         AssignNodes();
     }
@@ -110,11 +92,10 @@ public class NeuralNetwork : MonoBehaviour
     }
     void AssignInputs()
     {
-        inputs[0] = thrust;
+        inputs[0] = DirectionToGameobject();
         inputs[1] = progressToReproduce;
         inputs[2] = FieldOfView.visibleObjects.Count;
         inputs[3] = ShortestDistanceOfVisible();
-        inputs[4] = DirectionToGameobject();
     }
 
     // This method calculates the hypotenuse of a right triangle using the Pythagorean theorem
@@ -124,6 +105,7 @@ public class NeuralNetwork : MonoBehaviour
         return Mathf.Sqrt(
             (input1 * input1) + (input2 * input2)
             );
+
     }
 
     // This method calculates the shortest distance of visible gameobjects from the current gameobject
@@ -134,15 +116,19 @@ public class NeuralNetwork : MonoBehaviour
 
         // Initialize a variable to hold the index of the closest visible gameobject
         int closestDistanceIndex = -1;
-
-        // Loop through each visible gameobject
-        for (int i = 0; i < FieldOfView.visibleObjects.Count; i++)
+        if (FieldOfView.visibleObjects.Count != 0)
         {
-            // Calculate the distance from the current gameobject to the visible gameobject using the Pythagorean theorem
-            results.Add(PythagoreanTheorem(
-                Mathf.Abs(FieldOfView.visibleObjects[i].transform.position.x - transform.position.x),
-                Mathf.Abs(FieldOfView.visibleObjects[i].transform.position.y - transform.position.y)));
+            // Loop through each visible gameobject
+            for (int i = 0; i < FieldOfView.visibleObjects.Count; i++)
+            {
+                print(FieldOfView.visibleObjects.Count + " Objects");
+                // Calculate the distance from the current gameobject to the visible gameobject using the Pythagorean theorem
+                results.Add(PythagoreanTheorem(
+                    Mathf.Abs(FieldOfView.visibleObjects[i].transform.position.x - transform.position.x),
+                    Mathf.Abs(FieldOfView.visibleObjects[i].transform.position.y - transform.position.y)));
+            }
         }
+
 
         // Loop through the results to find the index of the closest visible gameobject
         for (int i = 0; i < results.Count; i++)
@@ -164,9 +150,18 @@ public class NeuralNetwork : MonoBehaviour
             // If there is no closest visible gameobject, set the closest gameobject to null
             closestGameobject = null;
         }
-
+        /*
+        if (Mathf.Min(results.ToArray()) == float.PositiveInfinity)
+        {
+            Debug.Break();
+        }
+        */
         // Return the shortest distance from the current gameobject to the visible gameobjects
-        return Mathf.Min(results.ToArray());
+        if (closestDistanceIndex != -1)
+        {
+            return Mathf.Min(results.ToArray());
+        }
+        return 0;
     }
 
     // This method calculates the direction from the current gameobject to the closest gameobject
@@ -193,38 +188,49 @@ public class NeuralNetwork : MonoBehaviour
     // This method assigns values to the nodes in a neural network
     void AssignNodes()
     {
-        // Create an array to store the values of the current layer's nodes
+        // Initialize two arrays of floating point numbers to hold the values of nodes and the values of nodes from the previous iteration
         float[] values = new float[7];
-
-        // Create an array to store the values of the last layer's nodes
         float[] lastValues = new float[7];
 
-        // Loop through each layer of the neural network
+        // Loop through each layer of nodes in the network
         for (int i = 0; i < 5; i++)
         {
-            // Loop through each node in the current layer
+            // Loop through each individual node in the current layer
             for (int r = 0; r < 7; r++)
             {
-                // Store the value of the current node
+                // Store the current value of the node in an array
                 values[r] = nodes[i][r].value;
 
-                // If we're not at the first layer of the network
+                // If the current layer is not the input layer, calculate the value of the current node based on the previous layer's values
                 if (i != 0)
                 {
-                    // Set the value of the current node to the sum of the last layer's node values
-                    nodes[i][r].value = Sum(lastValues);
+                    nodes[i][r].value = CalculateNodes(
+                        lastValues,
+                        nodes[i][r].inputWeights,
+                        nodes[i][r].bias);
                 }
-                // If we're at the first layer of the network
+                // If the current layer is the input layer, calculate the value of the current node based on the input values
                 else
                 {
-                    // Set the value of the current node to the sum of the input node values
-                    nodes[i][r].value = Sum(inputs);
+                    nodes[i][r].value = CalculateNodes(
+                        inputs,
+                        nodes[i][r].inputWeights,
+                        nodes[i][r].bias);
                 }
             }
 
-            // Copy the values of the current layer's nodes into the lastValues array
+            // Copy the values of the current layer's nodes to the lastValues array to be used in the next iteration
             Array.Copy(values, lastValues, values.Length);
         }
+
+        // Calculate the output value by summing the lastValues array
+        float outputValues = Sum(lastValues);
+
+        // Rotate the object using the output value and a smoothing factor
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(transform.rotation.z, outputValues, 0.05f));
+
+        // Set the thrust value based on the output value and a smoothing factor, clamping it between -3 and 5
+        thrust = Mathf.Lerp(thrust, Mathf.Clamp(outputValues, -3, 5), 0.25f);
     }
 
     void CreateNodes()
@@ -234,13 +240,21 @@ public class NeuralNetwork : MonoBehaviour
             nodes[i] = new Node[7];
             for (int r = 0; r < 7; r++)
             {
-                nodes[i][r].layer = i;
+                nodes[i][r].value = 0;
+                nodes[i][r].inputWeights = new float[7];
+                nodes[i][r].bias = 0;
+
             }
         }
     }
     // This method calculates the sum of the elements in a float array
     float Sum(float[] arguments)
     {
+        if (arguments.Length == 0)
+        {
+            return 0;
+        }
+
         // Initialize a variable to hold the sum
         float sum = 0;
 
@@ -254,12 +268,45 @@ public class NeuralNetwork : MonoBehaviour
         // Return the sum of the elements in the array
         return sum;
     }
-
+    void Reproduce()
+    {
+        progressToReproduce = 0;
+        if (isPrey)
+        {
+            Instantiate(SpawnOGAnimals.prey,
+                transform.position
+                + Vector3.up
+                * UnityEngine.Random.Range(-0.1f, 0.1f)
+                + Vector3.right
+                * UnityEngine.Random.Range(-0.1f, 0.1f),
+                Quaternion.identity, SpawnOGAnimals.emptyPrey);
+        }
+        if (isPredator)
+        {
+            Instantiate(SpawnOGAnimals.predator,
+                transform.position
+                + Vector3.up
+                * UnityEngine.Random.Range(-0.1f, 0.1f)
+                + Vector3.right
+                * UnityEngine.Random.Range(-0.1f, 0.1f),
+                Quaternion.identity, SpawnOGAnimals.emptyPredator);
+        }
+    }
+    float CalculateNodes(float[] values, float[] weights, float bias)
+    {
+        float[] alteredValues = new float[values.Length];
+        float result;
+        for (int i = 0; i < values.Length; i++)
+        {
+            alteredValues[i] = values[i] * weights[i];
+        }
+        result = Sum(alteredValues) + bias;
+        return result;
+    }
 }
 struct Node
 {
-    public float[] inputBiases;
+    public float bias;
     public float[] inputWeights;
     public float value;
-    public int layer;
 }
